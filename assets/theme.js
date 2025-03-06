a06e258392faa48a8cc80b8858a90a75e9198ea2
unknown <iftekharparvez174@gmail.com>
2025-03-06 09:46:03 +0600

    change some file

--- assets/theme.js
+++ assets/theme.js
@@ -31,4 +31,37 @@ $(document).ready(function(){
         document.getElementById('mobile-menu').classList.toggle('hidden');
     });
 
+
+
+
+
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tab-btn");
+    const contents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", function () {
+            let tabId = this.getAttribute("data-tab");
+
+            // Remove active state from all tabs
+            tabs.forEach(t => t.classList.remove("border-black", "font-semibold", "text-black"));
+            // Hide all tab contents
+            contents.forEach(c => c.classList.add("hidden"));
+
+            // Add active state to the clicked tab
+            this.classList.add("border-black", "font-semibold", "text-black");
+            // Show corresponding tab content
+            document.getElementById(tabId).classList.remove("hidden");
+
+
+            tabs.forEach(t => t.classList.remove("border-black", "font-semibold", "text-black"));
+            tabs.forEach(t => t.classList.add("border-transparent"));
+
+            // Add active styles to clicked button
+            this.classList.add("border-black", "font-semibold", "text-black");
+            this.classList.remove("border-transparent");
+
+        });
+    });
 });
